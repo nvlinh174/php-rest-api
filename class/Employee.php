2273dd7313ae342700676239b0f1146ddcca97c6
linhnv-dev <nvlinh17041992@gmail.com>
2023-10-27 10:33:44 +0700

    Concept of Insert Method

--- class/Employee.php
+++ class/Employee.php
@@ -23,6 +23,20 @@ class Employee
     // insert
     public function store()
     {
+        $sql = "INSERT INTO {$this->table} (name, email, address) VALUES (?, ?, ?)";
+        if ($statement = $this->connection->prepare()) {
+            $this->name = htmlspecialchars(strip_tags($this->name));
+            $this->email = htmlspecialchars(strip_tags($this->email));
+            $this->address = htmlspecialchars(strip_tags($this->address));
+
+            $statement->bind_param("sss", $this->name, $this->email, $this->address);
+
+            if ($statement->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
     // get one
